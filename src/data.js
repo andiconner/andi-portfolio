7f66a447817f37ce320847685684b17f67d9ca2a
Andi Conner <andiconner@icloud.com>
2022-03-30 21:30:11 -0400

    mern project

--- src/data.js
+++ src/data.js
@@ -20,62 +20,67 @@ export const projects = [
     },
     {
         id: 4,
-        title: "Brand & Web Design - Lab870",
-        link: "",
+        title: "MERN project - Alvandi Books",
+        link: "https://alvandi-books.herokuapp.com/",
         img: "4",
     },
     {
         id: 5,
-        title: "Brand & Web Design - Pecan's",
-        link: "http://pecansdayspacoralgables.com/",
+        title: "INTERACTIVE FULL-STACK PROJECT - Start Us",
+        link: "https://start-us-team-borderline.herokuapp.com/",
         img: "5",
     },
-    
     {
         id: 6,
-        title: "Brand & Web Design - Results Medspa",
-        link: "",
+        title: "INTERACT. FRONT-END PROJECT - Shelter",
+        link: "https://rebeccadherrera.github.io/shellter/",
         img: "6",
     },
-   
     {
         id: 7,
-        title: "Brand Design - Pepper's Salon",
-        link: "",
+        title: "MVC PROJECT - TechTalk",
+        link: "https://tech-talk-by-ac.herokuapp.com/",
         img: "7",
     },
     {
         id: 8,
-        title: "Brand Design - Canton",
-        link: "",
+        title: "SERVER-SIDE API PROJECT Weather Dashboard",
+        link: "https://andiconner.github.io/weather-dashboard/",
         img: "8",
     },
    
     {
         id: 9,
-        title: "SERVER-SIDE API PROJECT Weather Dashboard",
-        link: "https://andiconner.github.io/weather-dashboard/",
+        title: "Brand Design - Canton",
+        link: "",
         img: "9",
     },
     {
         id: 10,
-        title: "INTERACTIVE FULL-STACK PROJECT - Start Us",
-        link: "https://start-us-team-borderline.herokuapp.com/",
+        title: "Brand Design - Pepper's Salon",
+        link: "",
         img: "10",
     },
+   
     {
         id: 11,
-        title: "MVC PROJECT - TechTalk",
-        link: "https://tech-talk-by-ac.herokuapp.com/",
+        title: "Brand & Web Design - Results Medspa",
+        link: "",
         img: "11",
     },
     
     {
         id: 12,
-        title: "INTERACT. FRONT-END PROJECT - Shelter",
-        link: "https://rebeccadherrera.github.io/shellter/",
+        title: "Brand & Web Design - Pecan's",
+        link: "http://pecansdayspacoralgables.com/",
         img: "12",
     },
    
+    {
+        id: 13,
+        title: "Brand & Web Design - Lab870",
+        link: "",
+        img: "13",
+    },
 
 ]
