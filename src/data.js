41dd07c5ba554cfd0c211d0f2f1bd331a4dc9840
Andi Conner <andiconner@icloud.com>
2024-06-11 12:12:24 -0400

    data

---
+++ src/data.js
@@ -0,0 +1,68 @@
+export const projects = [
+    {
+        id: 1,
+        title: "Website - AB One",
+        link: "http://aboneflorida.com/",
+        img: "2",
+    },
+    {
+        id: 2,
+        title: "Brand & Web Design - Lab870",
+        link: "",
+        img: "4",
+    },
+    {
+        id: 3,
+        title: "Brand & Web Design - Pecan's",
+        link: "http://pecansdayspacoralgables.com/",
+        img: "5",
+    },
+    
+    {
+        id: 4,
+        title: "Brand & Web Design - Results Medspa",
+        link: "",
+        img: "6",
+    },
+   
+    {
+        id: 5,
+        title: "Brand Design - Pepper's Salon",
+        link: "",
+        img: "7",
+    },
+    {
+        id: 6,
+        title: "Brand Design - Canton",
+        link: "",
+        img: "8",
+    },
+   
+    {
+        id: 7,
+        title: "SERVER-SIDE API PROJECT Weather Dashboard",
+        link: "https://andiconner.github.io/weather-dashboard/",
+        img: "9",
+    },
+    {
+        id: 8,
+        title: "INTERACTIVE FULL-STACK PROJECT - Start Us",
+        link: "https://start-us-team-borderline.herokuapp.com/",
+        img: "10",
+    },
+    {
+        id: 9,
+        title: "MVC PROJECT - TechTalk",
+        link: "https://tech-talk-by-ac.herokuapp.com/",
+        img: "11",
+    },
+    
+    {
+        id: 10,
+        title: "INTERACT. FRONT-END PROJECT - Shelter",
+        link: "https://rebeccadherrera.github.io/shellter/",
+        img: "12",
+    },
+   
+
+]
